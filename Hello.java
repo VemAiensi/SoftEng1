cc88f3d4447d3e02ab02ace9cd1e3259bbd81949
Vem Aiensi Marasigan <142372590+VemAiensi@users.noreply.github.com>
2023-08-16 12:15:40 +0800

    Create Hello.java

---
+++ Hello.java
@@ -0,0 +1,6 @@
+public class Hello{
+  public static void main(String [] args){
+    System.out.printLn("Hello, Software Engineering Course!");
+    System.out.printLn("My name is MARASIGAN, Vem Aiensi!"");
+  }
+}
