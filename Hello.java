7b20296086dc99c45981b5ee6951e8c035eb13db
Vem Aiensi <vem.aiensi@gmail.com>
2023-08-26 17:39:59 +0800

    Made some changes in Hello.java

--- Hello.java
+++ Hello.java
@@ -3,5 +3,6 @@ public class Hello{
     System.out.println("Hello, Software Engineering Course!");
     System.out.println("My name is MARASIGAN, Vem Aiensi!");
     System.out.println("That is why developers are higly paid!");
+    System.out.println("Complicated world of SoftDev that is why it pays much!");
   }
 }
